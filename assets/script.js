a07ac074c602b780698cb37546e7163d0200a756
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-16 01:18:32 +0330

    used some guys projects idea for js and changed it in some parts

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,47 @@
 "use strict";
+
+let color = "black";
+
+divCreation(16);
+
+function divCreation(size) {
+  let container = document.querySelector(".container");
+  let squares = container.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  //i dont know how these work technically found em somwhere else
+  container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+  let gridSize = size * size;
+  for (let i = 0; i < gridSize; i++) {
+    let square = document.createElement("div");
+    square.classList.add("cell");
+    container.style.backgroundColor = "#e0e0e0";
+    square.addEventListener("mouseover", colorSquare);
+    container.insertAdjacentElement("beforeend", square);
+  }
+}
+
+function colorSquare() {
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "#000000";
+  }
+}
+
+function resetContainer() {
+  let container = document.querySelector(".container");
+  let squares = container.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "#e0e0e0"));
+}
+
+function changeSize(input) {
+  input >= 2 && input <= 100
+    ? divCreation(input)
+    : console.log("input error! to many or to few squares");
+}
+
+function changeColor(choice) {
+  color = choice;
+}
