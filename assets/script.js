71516616c7d08242b4862bea2009b316221d43d6
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-16 02:08:13 +0330

    changed some parts of the code

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//setting up the defualt
 let color = "black";
 
 divCreation(16);
@@ -8,6 +9,7 @@ function divCreation(size) {
   let container = document.querySelector(".container");
   let squares = container.querySelectorAll("div");
   squares.forEach((div) => div.remove());
+
   //i dont know how these work technically found em somwhere else
   container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
@@ -22,8 +24,14 @@ function divCreation(size) {
   }
 }
 
+//changing the color based on the users choice
+function changeColor(choice) {
+  color = choice;
+}
+
+//setting up a function to change the bg color of divs inside the sketchboard based on users choice
 function colorSquare() {
-  if (color == "random") {
+  if (color == "rainbow") {
     this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
   } else if (color == "black") {
     this.style.backgroundColor = "#000000";
@@ -34,18 +42,21 @@ function colorSquare() {
   }
 }
 
+//function for the reset button
 function resetContainer() {
   let container = document.querySelector(".container");
   let squares = container.querySelectorAll("div");
   squares.forEach((div) => (div.style.backgroundColor = "#e0e0e0"));
+  divCreation(16);
 }
 
+//function for changing grid numbers
 function changeSize(input) {
-  input >= 2 && input <= 100
-    ? divCreation(input)
-    : console.log("input error! to many or to few squares");
-}
-
-function changeColor(choice) {
-  color = choice;
+  if (input >= 2 && input <= 100) {
+    divCreation(input);
+  } else if (input > 100) {
+    alert("input error! too many squares !!!");
+  } else {
+    alert("input error! too few squares !!!");
+  }
 }
